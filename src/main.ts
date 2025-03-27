1ab0604e3c9072a80119489fd4737fa38d523419
Gustavolskw <gustavolschmidt13@gmail.com>
2025-03-26 21:19:26 -0300

    fix some shit

--- src/main.ts
+++ src/main.ts
@@ -21,7 +21,7 @@ const router = createRouter({
       path: '/',
       component: SidebarLayout,
       children: [
-        { path: '', redirect: '/login' },
+        { path: '', redirect: '/empresas' },
         { path: 'dashboard', component: Dashboard },
         { path: 'empresas', component: EmpresaForm },
         { path: 'usuarios', component: UsuarioForm },
